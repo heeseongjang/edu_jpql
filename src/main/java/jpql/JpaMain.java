9c5fb9fdcc1812657e4ca876324fd72c1cfa4dd8
heeseong <hsung0823@hanmail.net>
2021-10-08 22:28:37 +0900

    * 프로젝션 - 스칼라 - 임베디드 - 엔티티 - 여러 값 조회   > Query 타입   > Object[] 타입   > new 명령어

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -20,23 +20,40 @@ public class JpaMain {
             member.setUsername("member1");
             em.persist(member);
 
-            //반환 값이 명확할 때
-            TypedQuery<Member> query = em.createQuery("select m from Member m ", Member.class);
-            List<Member> resultList = query.getResultList();
+//            jpqlInit(em);
 
-            //무조건 결과가 1개여야한다. 그 이상도 그이하도 안된다(에러)
-            TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username =: username", Member.class);
-            //이름 기준으로 파라미터 바인딩
-            query3.setParameter("username", "member1");
-            Member singleResult = query3.getSingleResult();
+            em.flush();
+            em.clear();
+            //엔티티 프로젝션은 영속성 컨텍스트에 관리 대상이다!
+            List<Member> results = em.createQuery("select m from Member m ", Member.class).getResultList();
+            Member findMember = results.get(0);
+            findMember.setAge(20);
 
-            //반환 값이 명확하지 않을 떄
-            Query query2 = em.createQuery("select m.username, m.age from Member m"");
+
+            List<MemberDTO> results2 = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m ", MemberDTO.class).getResultList();
+
+            System.out.println(results.get(0).getUsername());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
             em.clear();
         }
+    }
+
+    private static void jpqlInit(EntityManager em) {
+        //반환 값이 명확할 때
+        TypedQuery<Member> query = em.createQuery("select m from Member m ", Member.class);
+        List<Member> resultList = query.getResultList();
+
+        //무조건 결과가 1개여야한다. 그 이상도 그이하도 안된다(에러)
+        TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username =: username", Member.class);
+        //이름 기준으로 파라미터 바인딩
+        query3.setParameter("username", "member1");
+        Member singleResult = query3.getSingleResult();
+
+        //반환 값이 명확하지 않을 떄
+        Query query2 = em.createQuery("select m.username, m.age from Member m");
+
     }
 }
